4dfe849d2748bb120f47e1fd8f607f21fd93b790
Ashna PS <77959009+ashnaps@users.noreply.github.com>
2022-10-04 09:17:55 +0530

    Update day7.cpp

--- day7.cpp
+++ day7.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 int  main()
 {
-    char A[1000000];
     int n;
     cin>>n;
+    vector<int> A(n);
     for(int i=0; i<n;i++)
     cin>>A[i];
     for(int i=n-1;i>=0;i--)
     cout<<A[i]<<" ";
+    cout<<endl;
+    return 0;
 }
