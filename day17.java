4b81ddeeeae6cde18aebbee1e51d78605e83a7ea
Ashna PS <77959009+ashnaps@users.noreply.github.com>
2022-10-07 14:40:26 +0530

    Update day17.java
    throws throw exception

--- day17.java
+++ day17.java
@@ -4,8 +4,7 @@ import java.lang.Math;
 
 
 class Calculator
-{
-    
+{   
     int power(int n, int p) throws Exception{
         if(n<0 || p<0)
         throw new Exception("n and p should be non-negative");
